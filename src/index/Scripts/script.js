1197e958dd52fe7fad376bb153079dbdd6f6a2e9
ARJUN AGARWAL <123585663+sahhoArjun097@users.noreply.github.com>
2024-09-14 01:01:43 +0530

    hopeso

--- src/index/Scripts/script.js
+++ src/index/Scripts/script.js
@@ -3,44 +3,44 @@ var curser = document.querySelector("#curser")
 var myimage = document.querySelector("#rigth-box")
 var comicbtn = document.querySelectorAll("#comic-button")
 
-main.addEventListener("mousemove", function(deft) {
-   gsap.to(curser,{
-    x:deft.x,
-    y:deft.y,
-    duration:1
-   })
+main.addEventListener("mousemove", function (deft) {
+  gsap.to(curser, {
+    x: deft.x,
+    y: deft.y,
+    duration: 1
+  })
 });
-myimage.addEventListener("mouseenter", function(){
-  curser.innerHTML="Hi!"
+myimage.addEventListener("mouseenter", function () {
+  curser.innerHTML = "Hi!"
 
-  gsap.to(curser,{
-  scale:1.5,
-  backgroundColor:"#eeeeee62"
+  gsap.to(curser, {
+    scale: 1.5,
+    backgroundColor: "#eeeeee62"
   })
 })
-myimage.addEventListener("mouseout", function(){
-  curser.innerHTML=" "
-  gsap.to(curser,{
-    scale:1, 
-    
-    backgroundColor:"#eee"
+myimage.addEventListener("mouseout", function () {
+  curser.innerHTML = " "
+  gsap.to(curser, {
+    scale: 1,
+
+    backgroundColor: "#eee"
   })
 })
 
 // --------------------------------------------------------------
 
 
-comicbtn.forEach(function(link) {
-  link.addEventListener("mouseenter", function() {
-    gsap.to(curser,{
-      scale:0,
-      })
+comicbtn.forEach(function (link) {
+  link.addEventListener("mouseenter", function () {
+    gsap.to(curser, {
+      scale: 0,
+    })
   });
 
-  link.addEventListener("mouseout", function() {
-    gsap.to(curser,{
-      scale:1, 
-      backgroundColor:"#eee"
+  link.addEventListener("mouseout", function () {
+    gsap.to(curser, {
+      scale: 1,
+      backgroundColor: "#eee"
     })
   });
 });
--- src/index/Scripts/script1.js
+++ src/index/Scripts/script1.js
@@ -1,71 +1,71 @@
 var tl = gsap.timeline()
-tl.from(".l-nav a",{
-  y:-20,
-  duration:1,
-  opacity:0,
-  delay:0.2
+tl.from(".l-nav a", {
+  y: -20,
+  duration: 1,
+  opacity: 0,
+  delay: 0.2
 })
 
-tl.from(".description .nav-skill",{
-  y:-20,
-  stagger:0.3,
-  opacity:0,
-  duration:1
+tl.from(".description .nav-skill", {
+  y: -20,
+  stagger: 0.3,
+  opacity: 0,
+  duration: 1
 })
 
-tl.from(".description .nav-work",{
-  y:-20,
-  stagger:0.3,
-  opacity:0,
-  duration:1
+tl.from(".description .nav-work", {
+  y: -20,
+  stagger: 0.3,
+  opacity: 0,
+  duration: 1
 })
 
-tl.from(".description .nav-edu",{
-  y:-20,
-  stagger:0.3,
-  opacity:0,
-  duration:1
+tl.from(".description .nav-edu", {
+  y: -20,
+  stagger: 0.3,
+  opacity: 0,
+  duration: 1
 })
 
-tl.from(".description .nav-resum",{
-  y:-20,
-  stagger:0.3,
-  opacity:0,
-  duration:1
+tl.from(".description .nav-resum", {
+  y: -20,
+  stagger: 0.3,
+  opacity: 0,
+  duration: 1
 })
 
-tl.from(".description .nav-contact",{
-  y:-20,
-  stagger:0.3,
-  opacity:0,
-  duration:1
+tl.from(".description .nav-contact", {
+  y: -20,
+  stagger: 0.3,
+  opacity: 0,
+  duration: 1
 })
-var cross =document.querySelector(".sheet-content i")
+var cross = document.querySelector(".sheet-content i")
 var menu = document.querySelector("#present-midnav #menuButton")
 
 var til = gsap.timeline()
-til.to(".menu-sheet",{
-  right:0,
-  duration:1
+til.to(".menu-sheet", {
+  right: 0,
+  duration: 1
 })
-til.from(".sheet-content h4",{
-  x:100,
-  duration:1,
-  stagger:0.3,
-  opacity:0,
+til.from(".sheet-content h4", {
+  x: 100,
+  duration: 1,
+  stagger: 0.3,
+  opacity: 0,
 })
-til.from(".sheet-content i",{
-  opacity:0
+til.from(".sheet-content i", {
+  opacity: 0
 })
 
 til.pause()
 
 
 
-menu.addEventListener("click",function(){
+menu.addEventListener("click", function () {
   til.play()
 })
 
-cross.addEventListener("click",function(){
+cross.addEventListener("click", function () {
   til.reverse()
 })
